d0c723e4d40f869d310701054a0b4721e925c18f
adam <ahanane191@gmail.com>
2024-12-12 17:51:42 +0100

    test

--- model/streamlit/main.py
+++ model/streamlit/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from st_on_hover_tabs import on_hover_tabs
 import plotly.express as px
 
-csv_path = os.path.join(os.path.dirname(__file__), 'sentiments_mappés_output_final.csv')
+csv_path = os.path.join('datacamp_efrei', 'model', 'streamlit', 'sentiments_mappés_output_final.csv')
 df = pd.read_csv(csv_path, delimiter=';')
 
 st.set_page_config(layout="wide")
