bb927239ca6b6aa85e3304280c6d24c376eebb0d
adam <ahanane191@gmail.com>
2024-12-13 16:04:14 +0100

    Fix name file

--- model/streamlit/main.py
+++ model/streamlit/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from st_on_hover_tabs import on_hover_tabs
 import plotly.express as px
 
-csv_path = os.path.join(os.path.dirname(__file__), 'luggage_predicted_review_pipeline.csv')
+csv_path = os.path.join(os.path.dirname(__file__), 'luggage_predicted_review_tokenizer.csv')
 df = pd.read_csv(csv_path, delimiter=',')
 
 st.set_page_config(layout="wide")
